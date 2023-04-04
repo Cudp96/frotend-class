de441823b0c7453ed5e32261972f8e4dbc082da1
Sudeep Shrestha <sudeepstha.96@gmail.com>
2023-04-04 19:55:43 +0545

    calculator

---
+++ java/java.js
@@ -0,0 +1,12 @@
+let a = "Sudeep Shrestha"
+console.log(a)
+
+const dict ={
+    name: "Sudeep",
+    language:"JavaScript",
+    level:'Absloute Beginner'
+}
+dict['anotherLanguage'] = 'Python'
+dict['name'] = 'cudp'
+console.log(dict.name)
+console.log(dict)
